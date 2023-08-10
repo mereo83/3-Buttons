4a4f91ee2e2fb3ea1a3a47b60cf9e1f8919529ca
mereo83 <mereo83@gmail.com>
2023-08-10 21:19:56 +0400

    updated js

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-const modal1 = document.querySelector('.modal-sec1'); //reference of popup1 <element></element>
-const modal2 = document.querySelector('.modal-sec2'); //reference of popup2 <element></element>
-const modal3 = document.querySelector('.modal-sec3'); //reference of popup3 <element></element>
-const overlay1 = document.querySelectorAll('.overlay'); //common overlay class [<element></element>]
-const btnCloseModal = document.querySelectorAll('.close-modal'); // close model button [<element/>]
-const btnsOpenModal = document.querySelectorAll('.show-modal'); // open model button [<element/>]
+const modals = document.querySelectorAll('.modal');
+const overlays = document.querySelectorAll('.overlay');
+const btnCloseModal = document.querySelectorAll('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-// opening all the 3 popup + all the 3 overlays
-let openModel = function (i) {
-  if (i == 0) modal1.classList.remove('hidden');
-  else if (i == 1) modal2.classList.remove('hidden');
-  else if (i == 2) modal3.classList.remove('hidden');
-  overlay1[i].classList.remove('hidden');
-};
+function openModel(i) {
+  modals[i].classList.remove('hidden');
+  overlays[i].classList.remove('hidden');
+}
 
-// close all the 3 popup + all the 3 overlays
-let closeModel = function (i) {
-  if (i == 0) modal1.classList.add('hidden');
-  else if (i == 1) modal2.classList.add('hidden');
-  else if (i == 2) modal3.classList.add('hidden');
-  overlay1[i].classList.add('hidden');
-};
+function closeModel(i) {
+  modals[i].classList.add('hidden');
+  overlays[i].classList.add('hidden');
+}
 
-for (let i = 0; i < btnsOpenModal.length; i++)
+for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', () => openModel(i));
+}
 
-for (let j = 0; j < btnCloseModal.length; j++)
+for (let j = 0; j < btnCloseModal.length; j++) {
   btnCloseModal[j].addEventListener('click', () => closeModel(j));
+}
